d5fb7581434568ba301c5c6fc980560017dd999e
Reynard Timotius <91584193+Xhourstoprogram@users.noreply.github.com>
2022-03-04 17:23:29 +0700

    Add files via upload

---
+++ number8.java
@@ -0,0 +1,18 @@
+package com.OOP;
+
+public class number8 {
+    public static void main(String[] args) throws Exception {
+        System.out.println ("This Application Displays A Box, An Oval, An Arrow"
+                + " And A Diamond Using Asterisks (*)");
+         
+        System.out.print ("*********      ***        *          * \n");
+        System.out.print ("*       *    *     *     ***        * * \n");
+        System.out.print ("*       *   *       *   *****      *   * \n");
+        System.out.print ("*       *   *       *     *       *     * \n");
+        System.out.print ("*       *   *       *     *      *       * \n");
+        System.out.print ("*       *   *       *     *       *     * \n");
+        System.out.print ("*       *   *       *     *        *   * \n");
+        System.out.print ("*       *    *     *      *         * * \n");
+        System.out.print ("*********      ***        *          * \n");  }
+}
+
